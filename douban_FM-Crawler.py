ec63a91302ee77837e29922548adb8e90e0d4bcb
Cornelius194968 <Cornelius194968@gmail.com>
2014-11-17 16:00:48 -0700

    Add txt file, add .DS_Store to ignore file.

--- douban_FM-Crawler.py
+++ douban_FM-Crawler.py
@@ -1,4 +1,4 @@
-import urllib
+#import urllib
 import urllib2
 import re
 
@@ -7,11 +7,40 @@ class douban_FM:
     "This is a python crawler to crawl the popular music on Douban FM."
 
     def __init__(self):
-        self.url = "http://music.douban.com/subject/"
-        self.getData()
+        self.url = "http://music.douban.com/musician/"
+        self.currentPage = 100001
+        self.thresholdValue = 100
+        self.txtFile = open('douban_Data.txt', 'w')
 
     def getData(self):
-        pageInfo = urllib2.urlopen(self.url + "3533452").read()
-        print(pageInfo)
+        try:
+            pageInfo = urllib2.urlopen(self.url + str(self.currentPage)).read()
+            pageInfo = pageInfo.decode("utf-8")
+            numberMatch = self.currentPage
+            titleMatch = re.search('<title>(.*?)\\|.*?</title>', pageInfo, re.S)
+            contentMatch = re.findall(r'<div class="col song-name-short" data-title="(.*?)">.*?<span class="n_doulists unfoldable">(\d+).*?</span>', pageInfo, re.S)
+            data = {}
+            for i in contentMatch:
+                if int(i[1]) >= self.thresholdValue:
+                    data.update({i[0]: i[1]})
+            if len(data) > 0:
+                print titleMatch.group(1) + "\t" + str(numberMatch)
+                #print a
+                #self.txtFile.write(a + "\n")
+                for i in data:
+                    print i, "\t", data[i]
+                    #print b
+                    #self.txtFile.write(b + "\n")
+        except urllib2.URLError as e:
+            pass
+
+    def multiPage(self):
+            num = 0
+            while num <= 10000:
+                self.currentPage = 100000 + num
+                self.getData()
+                num = num + 1
+            #self.txtFile.close()
 
 douban = douban_FM()
+douban.multiPage()
